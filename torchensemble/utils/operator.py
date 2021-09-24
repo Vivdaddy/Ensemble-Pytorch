5939817f31416ae1a71f1822f8fe338d2a2780ed
Vivek Anand <vivek2000anand@gmail.com>
2021-09-24 01:34:06 -0400

    fixed torch tensor issue

--- torchensemble/utils/operator.py
+++ torchensemble/utils/operator.py
@@ -26,13 +26,13 @@ def averaged_std(outputs):
     Step 2: Average Across all datapoints
     Step 3: Average Across all item
     """
-    print("Outputs ", outputs)
+    #print("Outputs ", outputs)
     outputs = torch.stack(outputs)
-    print("Stacked outputs ", outputs)
+    #print("Stacked outputs ", outputs)
     step1 = torch.std(outputs, dim=0)
     step2 = torch.mean(step1, dim=0)
     step3 = torch.mean(step2, dim=0)
-    print("step 3 is ", step3)
+    #print("step 3 is ", step3)
     return step3.tolist()
 
 
